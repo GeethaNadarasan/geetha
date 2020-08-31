4cb2e9790f1464cf8a50fb45968143299c2b2d51
GeethaNadarasan <61857515+GeethaNadarasan@users.noreply.github.com>
2020-08-31 14:54:59 +0800

    Create RoosterTest.java

---
+++ src/main/java/test/RoosterTest.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class RoosterTest {
+	
+	@Test
+	public void testRoosterService(){
+		Rooster rooster = new Rooster();
+		rooster.setFlyBehaviour(new FlyWithWings());
+		Assert.assertEquals("I am flying",rooster.performFly());
+		
+		rooster.setSoundBehaviour(new RoosterSound());
+		Assert.assertEquals("Cock-a-doodle-doo",rooster.performSound());
+		
+		rooster.walk();
+	}
+
+}
