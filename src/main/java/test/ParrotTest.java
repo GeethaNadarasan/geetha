15a5467189cef6fff6377b82afc0f6b2cb810e1d
GeethaNadarasan <61857515+GeethaNadarasan@users.noreply.github.com>
2020-08-31 14:55:55 +0800

    Create ParrotTest.java

---
+++ src/main/java/test/ParrotTest.java
@@ -0,0 +1,22 @@
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class ParrotTest {
+	
+	@Test
+	public void testRoosterSound(){
+		Parrot parrot = new Parrot();
+		parrot.setSoundBehaviour(new DogSound());
+		Assert.assertEquals("Woof, woof", parrot.performSound());
+		parrot.setSoundBehaviour(new DuckSound());
+		Assert.assertEquals("Quack, quack", parrot.performSound());
+		parrot.setSoundBehaviour(new CatSound());
+		Assert.assertEquals("Meow", parrot.performSound());
+		parrot.setSoundBehaviour(new RoosterSound());
+		Assert.assertEquals("Cock-a-doodle-doo", parrot.performSound());
+		parrot.setSoundBehaviour(new PhoneSound());
+		//Assert.assertEquals("", parrot.performSound());
+	}
+
+}
