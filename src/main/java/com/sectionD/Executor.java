5a9b834c247120a3c695443bfe6051dd35b7c3a0
GeethaNadarasan <61857515+GeethaNadarasan@users.noreply.github.com>
2020-08-31 14:32:14 +0800

    Create Executor.java

---
+++ src/main/java/com/sectionD/Executor.java
@@ -0,0 +1,16 @@
+package com.sectionD;
+
+public class Executor {
+	public static void main(String args[]){
+
+		Caterpillar caterpillar = new Caterpillar();
+		caterpillar.walk();
+		caterpillar.fly();
+		
+		caterpillar.setFlyBehaviour(new Butterfly());
+		caterpillar.setSoundBehaviour(new Butterfly());
+		
+		caterpillar.performAction();
+	}
+
+}
