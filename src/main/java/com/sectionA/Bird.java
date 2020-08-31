9a631c442c18175abc85d4e1e802f70e5066abe4
GeethaNadarasan <61857515+GeethaNadarasan@users.noreply.github.com>
2020-08-31 12:30:23 +0800

    Create Bird.java

---
+++ src/main/java/com/sectionA/Bird.java
@@ -0,0 +1,13 @@
+package com.singtel.sectionA;
+
+public class Bird extends Animal {
+	
+	@Override
+	public void walk(){
+		System.out.println("I am walking");
+	}
+	
+	public void fly(){
+		System.out.println("I am flying");
+	}
+	
